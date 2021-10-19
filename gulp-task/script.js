407e671ddd1ac40a647c6b02c7914b0a3f9d8cae
Droplan <49207656+Droplan@users.noreply.github.com>
2021-10-19 20:55:31 +0400

    Changing html task

--- gulp-task/script.js
+++ gulp-task/script.js
@@ -1,30 +1,19 @@
 const { src, dest } = require("gulp");
 const browserSync = require("browser-sync").create();
 const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
 const uglify = require("gulp-uglify");
 
 const { paths } = require("./paths");
 
 function scriptHandler(input, output) {
   return src(input)
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(function (err) {
-          return {
-            title: "Styles",
-            meassage: err.message,
-          };
-        }),
-      })
-    )
+    .pipe(plumber())
     .pipe(
       uglify({
         toplevel: true,
       })
     ) // Оптимизирует и минифицирует JS
-    .pipe(dest(output))
-    .pipe(browserSync.stream());
+    .pipe(dest(output));
 }
 
 // Функции для тасков
--- gulp-task/style.js
+++ gulp-task/style.js
@@ -2,7 +2,6 @@
 const { src, dest } = require("gulp");
 const browserSync = require("browser-sync").create();
 const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
 const autoprefixer = require("gulp-autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 const cleanCSS = require("gulp-clean-css");
@@ -12,21 +11,21 @@ const { paths } = require("./paths");
 
 function styleHandler(input, output, preprocessor) {
   return src(input) // Берёт файлы из указанной директории
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(function (err) {
-          return {
-            title: "Styles",
-            meassage: err.message,
-          };
-        }),
-      })
-    )
+    .pipe(plumber())
     .pipe(sourcemaps.init()) // Создаёт карту источников CSS
-    .pipe(preprocessor()) // Выполняет преобразование less или scss/saas в CSS
+    .pipe(preprocessor) // Выполняет преобразование less или scss/saas в CSS
     .pipe(
       autoprefixer({
-        cascade: false,
+        overrideBrowserslist: ["last 8 versions"],
+        browsers: [
+          "Android >= 4",
+          "Chrome >= 20",
+          "Firefox >= 24",
+          "Explorer >= 11",
+          "iOS >= 6",
+          "Opera >= 12",
+          "Safari >= 6",
+        ],
       })
     ) // Расставляет автопрефиксы для браузеров
     .pipe(
@@ -35,16 +34,19 @@ function styleHandler(input, output, preprocessor) {
       })
     ) // Оптимизирует и минифицирует CSS
     .pipe(sourcemaps.write()) // Добавляет карту источников CSS
-    .pipe(dest(output)) // Сохраняет файл в указанную директорию
-    .pipe(browserSync.stream()); // Обновляет открытую страницу
+    .pipe(dest(output)); // Сохраняет файл в указанную директорию
 }
 
 // Функции для тасков
 
 module.exports.styleLess = function () {
-  return styleHandler(paths.src.style.less, paths.build.css, less);
+  return styleHandler(paths.src.style.less, paths.build.css, less());
 };
 
 module.exports.styleSass = function () {
-  return styleHandler(paths.src.style.scss, paths.build.css, sass);
+  return styleHandler(
+    paths.src.style.scss,
+    paths.build.css,
+    sass({ outputStyle: "compressed" }).on("error", sass.logError)
+  );
 };
